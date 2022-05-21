3b627a7472aa42339b385b3f85aa7c6280dbc41e
sirjerr654 <danieljeremiah40@gmail.com>
2022-05-21 01:00:33 -0700

    update

---
+++ fun.js
@@ -0,0 +1,28 @@
+// variables and constants
+
+let x=5;
+y=6;
+z=7;
+console.log(x);
+console.log(y);
+console.log(z);
+
+const a=8;
+console.log(a); 
+// its recommended that if the value of the variable wont change use CONST
+
+let sum =44;
+console.log(sum);
+
+// console.log() Syntax
+console.log(" I love JS");
+
+// Program to print variables values
+// storing values
+const greet = 'Hello';
+const nam = 'Sirjerry';
+console.log(greet + ' ' + nam);
+
+// Access string characters
+const j = 'hello';
+console.log(j[1]);
